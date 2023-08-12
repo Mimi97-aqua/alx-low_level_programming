2ed0b4b670b0a18d504864f93316ee68d93e87d1
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-08-12 07:24:12 -0700

    Added description for main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * This program demonstartes a 
+ * simple c program that prints 
+ * text to standard output using
+ * the puts function.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
