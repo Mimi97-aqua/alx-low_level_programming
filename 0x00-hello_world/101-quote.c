704655f3b1ca8f31877f6cde5c5b77db3ad625a7
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-08-12 08:55:59 -0700

    Modifications

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, sizeof(message) - 1);
 	return (1);
 }
