2aa77e9e4910b3962192018a26d480d9633fb42f
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-08-31 06:58:10 -0700

    First commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: character to be checked
+ * Return: 1 if lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z') ? 1 : 0;
+}
