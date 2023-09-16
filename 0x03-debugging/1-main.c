1d593220c703fa3eb04fbdabba978a70ba644ea0
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-16 16:12:41 -0700

    Changes

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * Commented out the infinite code
-	 *
-	 * while (i < 10)
-	 * {
-	 *	putchar(i);
-	 * }
-	 */
+
+	 /* while (i < 10)*/
+	 /* {*/
+	 /*	putchar(i);*/
+	 /* }*/
+	 /*/*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
