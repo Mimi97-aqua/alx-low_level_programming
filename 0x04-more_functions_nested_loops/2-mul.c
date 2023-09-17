2c7437d6d44c62f93ce834e9e74af2c9ade85382
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-17 12:31:08 -0700

    Changes: 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,14 +3,10 @@
  *
  * @a: first integer
  * @b: second integer
- * @result: product of a and b
  *
  * Return: product of a and b
  */
-
 int mul(int a, int b)
 {
-	int result = a * b;
-
-	return (result);
+	return (a * b);
 }
