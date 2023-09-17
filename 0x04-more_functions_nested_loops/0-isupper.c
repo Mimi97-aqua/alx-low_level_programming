3b1190e5aa4d52d84bb22c43649683eb0308ca54
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-17 12:21:04 -0700

    First commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
