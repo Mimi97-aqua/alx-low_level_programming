8d9883f5caee1ff1d6a7ce382805191867aab507
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-22 06:26:01 -0700

    Initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * premain - prints a string before the main function is executed
+ *
+ * Return: void
+ */
+
+__attribute__ ((constructor))
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
