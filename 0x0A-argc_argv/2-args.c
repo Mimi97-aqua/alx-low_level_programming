843bc3991c4e43143e6d814d43ee293128c5c112
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-25 05:03:40 -0700

    2-args.c done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: An integer 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
