14152b04a6080699ab72d490c4898898aad82113
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-25 08:13:53 -0700

    Modified code

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ *
+ * @head: pointer to the head of the list
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
