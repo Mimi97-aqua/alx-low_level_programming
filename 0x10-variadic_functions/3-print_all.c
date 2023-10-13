ffebe399f1f6e17fc83dc3f1cee094c7a6fe70e7
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-10-13 08:29:26 -0700

    Modified code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,55 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
-* print_all - a function to print
-*
-* @format: format
-*
-* Return - nothing
-*/
+ * print_all - a function that prints anything.
+ *
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: Just Nothing.
+ */
 
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	unsigned int j = 0, start = 0;
-	char *p;
+	int indx, findic;
+	char *str;
+	va_list myapx;
 
-	va_start(list, format);
-	while (format && format[j] != '\0')
+	indx = 0;
+
+	va_start(myapx, format);
+	while (format != NULL && format[indx] != '\0')
 	{
-		switch (format[j])
-		{ case 'c':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%c", va_arg(list, int));
-			break;
+		switch (format[indx])
+		{
 			case 'i':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%i", va_arg(list, int));
-			break;
-		case 'f':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%f", va_arg(list, double));
-			break;
-		case's':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			p = va_arg(list, char*);
-			if (p)
-			{ printf("%s", p);
-			break; }
-			printf("%p", p);
-			break; }
-		j++;
+				printf("%i", va_arg(myapx, int));
+				findic = 0;
+				break;
+			case 's':
+				str = va_arg(myapx, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				findic = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(myapx, int));
+				findic = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(myapx, double));
+				findic = 0;
+				break;
+			default:
+				findic = 1;
+				break;
+		}
+		if (findic == 0 && format[indx + 1] != '\0')
+			printf(", ");
+		indx++;
 	}
-
 	printf("\n");
-	va_end(list);
+	va_end(myapx);
 }
