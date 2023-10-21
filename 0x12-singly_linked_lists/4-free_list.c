c673b3e040354f7b873be88ebce968512364a076
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-10-20 17:36:12 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list, including its nodes and strings.
+ 
+ * @head: A pointer to the head of the list
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
