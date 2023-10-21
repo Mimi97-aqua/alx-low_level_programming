718eeff1098a2b8191e949a668e154f1441eb33a
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-10-20 17:53:21 -0700

    Changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,12 @@
 #define DOG_H
 /**
  * struct dog - The Dog main struct.
+ *
  * @name: The dog name goes here.
  * @age: The age of that dog.
  * @owner: Who the owner? of the dog.
  */
+
 typedef struct dog
 {
 	char *name;
